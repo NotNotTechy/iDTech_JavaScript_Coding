c5854d81a5aa717e335b561eb5079dfa9781070c
NotNotTechy <142513792+NotNotTechy@users.noreply.github.com>
2023-08-18 11:59:29 -0400

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hello there!"
+let name = "Ken"
+let Class = "fighter"
+let background = "soldier"
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 21
+let year = 1350
+let century = 14
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsUsed = ["sword", "longbow", "shield", "dagger"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let afraidOfHeights = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " My name is " + name + ".")
+console.log("I'm a " + Class + ", traveling across a forgotten land.")
+console.log("I use many different weapons, but the one I use most is my " + weaponsUsed[0] + ".")
+console.log("Am I afraid of heights? Don't tell this to ANYONE but...")
+console.log("It's " + afraidOfHeights + ", I am afraid of heights.\n")
+console.log("I am " + age + " years old.")
+console.log("It's the year " + year + ", in the " + century + "th century.")
